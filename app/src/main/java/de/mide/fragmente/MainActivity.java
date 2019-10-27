9b8cc717b95affb78cbe0186aa2d5c8b78e14aca
MDecker-MobileComputing <mide_42b@yahoo.de>
2019-10-27 21:09:19 +0100

    Upgrade auf Android Studio 3.5

--- app/src/main/java/de/mide/fragmente/MainActivity.java
+++ app/src/main/java/de/mide/fragmente/MainActivity.java
@@ -18,27 +18,26 @@ import android.widget.Button;
 public class MainActivity extends Activity implements OnClickListener {
 
 	public static String TAG4LOGGING = "Fragmente_Demo";
-	
+
 	/** Button für Öffnen der Activity zur Berechnung der Entfernung nach Berlin. */
 	protected Button _berechneEntfernungNachBerlinButton = null;
 
     /** Button für Öffnen der Activity zur Berechnung der Entfernung zwischen zwei Orten. */
 	protected Button _berechneEntfernungZwischenZweiKoordinatenButton = null;
-	
-	
-	
+
+
 	/**
 	 * Lifecycle-Methode, lädt Layout-Datei und setzt Event-Handler für Buttons.
 	 */
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
-        
+
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-				
+
 		_berechneEntfernungNachBerlinButton = findViewById(R.id.button_entfernung_zu_berlin);
 		_berechneEntfernungNachBerlinButton.setOnClickListener(this);
-		
+
 		_berechneEntfernungZwischenZweiKoordinatenButton = findViewById(R.id.button_entfernung_zwischen_zwei_orten);
 		_berechneEntfernungZwischenZweiKoordinatenButton.setOnClickListener(this);
 	}
@@ -46,24 +45,24 @@ public class MainActivity extends Activity implements OnClickListener {
 
 	/**
 	 * Event-Handler für Button, einzige Methode aus Interface {@link OnClickListener}.
-	 * 
+	 *
 	 * @param view Objekt, das das Event ausgelöst hat (sollte ein Button sein).
 	 */
 	@Override
 	public void onClick(View view) {
-		
+
 		Intent intent = null;
-		
+
 		if (view == _berechneEntfernungNachBerlinButton) {
-			
+
 			intent = new Intent(this, EntfernungVonBerlinActivity.class);
 			startActivity(intent);
-			
+
 		} else if (view == _berechneEntfernungZwischenZweiKoordinatenButton) {
 
 			intent = new Intent(this, EntfernungZweiKoordinatenActivity.class);
-			startActivity(intent);						
-			
+			startActivity(intent);
+
 		} else {
 
 			Log.w(TAG4LOGGING, "Unerwartetes UI-Element hat Event-Handler ausgelöst: " + view);
